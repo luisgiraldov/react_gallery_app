481a6a010dedbbc80960ef4bbeb29af03aa7b0d7
Luis Giraldo <lugirva@gmail.com>
2020-05-14 16:28:58 -0400

    Added initial components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/index.css';
+import apiKey from './config.js';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// App components
+import SearchForm from './components/SearchForm.js';
+import Nav from './components/Nav.js';
+import PhotoGallery from './components/PhotoGallery';
+
+
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+      <SearchForm />
+      <Nav />
+
+      <Switch>
+        <Route exact path="/" render={ () => <PhotoGallery /> } />
+        <Route path="/cats" render={ () => <PhotoGallery /> } />
+      </Switch>
     </div>
-  );
-}
+  </BrowserRouter>
+);
+
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => (
+    <nav className="main-nav">
+        <ul>
+          <li><a href='#'>Dogs</a></li>
+          <li><a href='#'>Cats</a></li>
+          <li><a href='#'>Mountains</a></li>
+        </ul>
+    </nav>
+);
+
+export default Nav;
